c2bfd5aca18426659603cf61e38ef52086bc29ee
amburu4159 <boramatiti1@gmail.com>
2021-08-15 20:23:18 -0500

    saving R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,12 +1,25 @@
-#import readr
-library(readr)
+#import Libraries 
+#install.packages("broom", type="binary")
+#library(tidyverse)
 
-#read data into dataframe 
-mechaCarData <- read_csv("MechaCar_mpg.csv")
-head(mechaCarData)
+#read mechaCarMpg data into dataframe 
+mechaCarMpg <- read_csv("MechaCar_mpg.csv")
+head(mechaCarMpg)
 
 #linear regression model with the intended p-values
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarData) 
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarMpg) 
 
 #summary
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarData))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarMpg))
+
+#read suspension_coil data into dataframe
+suspension_coil <- read_csv("Suspension_Coil.csv") 
+head(suspension_coil)
+
+#total summary table
+total_summary <- suspension_coil%>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+total_summary
+
+#lot summary table
+lot_summary <- suspension_coil%>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+lot_summary
